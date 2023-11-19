b4ec8257681524f18615885654768fe4a6b4718f
Shivam Kumar Kashyap <109240141+ShivamKashyap@users.noreply.github.com>
2023-11-19 19:12:24 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <Routes>
+
+        <Route path='/' element={<>home page</>} />
+        <Route path='/about' element={<>About page</>} />
+        <Route path='/contact' element={<>contact page</>} />
+
+      </Routes>
+    </HashRouter>
+  )
 }
 
-export default App;
+export default App
