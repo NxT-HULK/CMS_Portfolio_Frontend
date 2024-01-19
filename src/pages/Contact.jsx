b50c038ac6352a27510e5f704ebaba4c811ac1fd
Shivam Kashyap <shivamkumarkashyap12@gmail.com>
2024-01-19 15:29:39 +0530

    Contact API Connections, Code Refactor

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { CustomBtn, FirstLetterEffectText } from '../components/Utility'
 import { BsFillSendFill } from 'react-icons/bs'
+import DataContext from '../context/data/DataContext'
+import FunctionContext from '../context/function/FunctionContext'
 
 const Contact = () => {
+
+    const { setResponseStatus, setResponseData, responseData, backendHost } = useContext(DataContext)
+    const { handleOnChange } = useContext(FunctionContext)
+
+    const contactForm = useRef("")
+    const [contactFormData, setContactFormData] = useState({})
+    const handleSubmitContact = async (e) => {
+        e.preventDefault()
+        try {
+
+            setResponseStatus(true)
+            setResponseData({ ...responseData, "isLoading": true, "heading": 'Sending Contact' })
+
+            let raw = await fetch(`${backendHost}/contact`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "name": contactFormData.name,
+                    "query": contactFormData.query,
+                    "email": contactFormData.email,
+                    "mess": contactFormData.mess
+                })
+            })
+
+            let data = await raw.json()
+
+            console.log(data)
+
+            setResponseData({ ...responseData, "isLoading": false, "heading": 'Sending Contact', "message": data[0].msg || data })
+
+            setTimeout(() => {
+                setResponseStatus(false)
+                setResponseData({})
+            }, 4000);
+
+            contactForm.current.reset()
+
+        } catch (error) {
+
+            setResponseData({ ...responseData, "isLoading": false, "heading": 'Sending Contact', "message": "Oops! Something went wrong on our server. Please try again after some time. Thank you for your patience." })
+            setTimeout(() => {
+                setResponseStatus(false)
+                setResponseData({})
+            }, 4000);
+
+            console.error(error.message)
+        }
+    }
+
     return (
         <>
             <section className="random-background py-5">
@@ -14,18 +67,18 @@ const Contact = () => {
 
                         <div className="col-md-6 col-12 p-md-3 px-2 rounded-4" style={{ background: '#6a59d115', backdropFilter: 'blur(7px)', boxShadow: '5px 5px 0 #6a59d130' }}>
                             <FirstLetterEffectText text={"Get In Touch"} className={'text-md-start text-center'} />
-                            <form action="" className='mt-3'>
+                            <form className='mt-3' onSubmit={handleSubmitContact} ref={contactForm}>
                                 <div className="mb-2">
-                                    <input type="text" className="rounded-1 custom-input-style" aria-describedby="NameHelp" placeholder="Your Name*" required />
+                                    <input type="text" name="name" className="rounded-1 custom-input-style" placeholder="Your Name*" required onChange={(e) => {handleOnChange(e, contactFormData, setContactFormData)}} />
                                 </div>
                                 <div className="mb-2">
-                                    <input type="text" className="rounded-1 custom-input-style" aria-describedby="QueryHelp" placeholder="Your Query*" required />
+                                    <input type="text" name="query" className="rounded-1 custom-input-style" placeholder="Your Query*" required onChange={(e) => {handleOnChange(e, contactFormData, setContactFormData)}} />
                                 </div>
                                 <div className="mb-2">
-                                    <input type="email" className="rounded-1 custom-input-style" aria-describedby="emailHelp" placeholder="youremail@domain.com*" />
+                                    <input type="email" name="email" className="rounded-1 custom-input-style" placeholder="youremail@domain.com*" onChange={(e) => {handleOnChange(e, contactFormData, setContactFormData)}} />
                                 </div>
                                 <div className="mb-2">
-                                    <textarea name="message" id="" rows="5" className='w-100 custom-input-style rounded-1' placeholder="Your Message*" data-gramm="false" data-gramm_editor="false" data-enable-grammarly="false" required />
+                                    <textarea name="mess" id="" rows="5" className='w-100 custom-input-style rounded-1' placeholder="Your Message*" required onChange={(e) => {handleOnChange(e, contactFormData, setContactFormData)}} />
                                 </div>
                                 <CustomBtn text="Send Message" icon={<BsFillSendFill />} type={'submit'} className={'mx-md-0 mx-auto'} />
                             </form>
